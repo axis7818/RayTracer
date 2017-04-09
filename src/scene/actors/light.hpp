eea5c501eb28fe4620b9e95dce0f77b2d35715f0
cameron <axis7818@gmail.com>
2017-04-09 13:57:47 -0700

    forgot light...

--- src/scene/actors/light.hpp
+++ src/scene/actors/light.hpp
@@ -2,4 +2,12 @@
 #ifndef _LIGHT_HPP
 #define _LIGHT_HPP
 
+#include <utils/color.hpp>
+#include "actor.hpp"
+
+class Light: public Actor {
+public:
+   RGBColor color;
+};
+
 #endif /* end of include guard: _LIGHT_HPP */
