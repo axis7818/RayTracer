4fbb482be2f27d6e0e20b96ff6e86905f2566ad9
cameron <axis7818@gmail.com>
2017-05-19 09:51:18 -0700

    fix build issues hopefully

--- src/scene/geometry/geometry.cpp
+++ src/scene/geometry/geometry.cpp
@@ -5,10 +5,10 @@ using namespace std;
 
 vec3 Geometry::point_to_obj_space(vec3 world_point) {
    vec4 point = vec4(world_point, 1.0f);
-   return inv_transform * point;
+   return vec3(inv_transform * point);
 }
 
 vec3 Geometry::normal_to_world_space(vec3 obj_normal) {
    vec4 normal = vec4(obj_normal, 0.0f);
-   return normalize(normal_matrix * normal);
+   return normalize(vec3(normal_matrix * normal));
 }
